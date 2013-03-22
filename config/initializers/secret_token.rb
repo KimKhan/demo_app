475ee38c5939dd8731573236bc05998aafe499e2
Kim Khan <kim.khan@lendkey.com>
2013-03-22 17:32:05 -0400

    Initial commit for demo app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'ee176082ecde8e88cea7ea840fd56bebf3743b5572305ee806fb58d97ab6f4d8feea7f75b3e42ef3bb4d1ae70572328b03f4a9e574303e6eb23fd33eedbee018'
